c396441c0d7d235d51da8d662f46b2c141b8be1f
Ademario Santana <ademario63@gmail.com>
2022-08-17 21:18:59 -0300

    feat: add right rules about monorepo

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
+const router = require('./routes/routes')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
