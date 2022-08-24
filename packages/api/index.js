07b3e3dd80de84cbe8b867b154aa12c3ede92b17
Ademario Santana <ademario63@gmail.com>
2022-08-24 17:28:44 -0300

    feat: add mongo and mongoose in project

--- packages/api/index.js
+++ packages/api/index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const router = require('./routes/routes')
 const cors = require('cors')
 const app = express()
+const mongoose = require('mongoose')
+const User = require('./schemas/User.schema')
+
+mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
+  console.log("conectou no mongo")
+}, e => console.log(e))
+
+async function run() {
+  const user = await User.create({name: "Ademario Vitor", age: 23 })
+  console.log(user)
+}
+
+run()
 
 app.use(cors({
   origin: '*'
