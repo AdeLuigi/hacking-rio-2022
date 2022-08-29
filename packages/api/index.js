3a10964a6b26d97d0137331d7a463d2e03bbfc67
Ademario Santana <ademario63@gmail.com>
2022-08-29 11:47:14 -0300

    change port heroku

--- packages/api/index.js
+++ packages/api/index.js
@@ -5,22 +5,22 @@ const app = express()
 const mongoose = require('mongoose')
 const User = require('./schemas/User.schema')
 
-// mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
-//   console.log("conectou no mongo")
-// }, e => console.log(e))
+mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
+  console.log("conectou no mongo")
+}, e => console.log(e))
 
-// async function run() {
-//   const user = await User.create({name: "Ademario Vitor", age: 23 })
-//   console.log(user)
-// }
+async function run() {
+  const user = await User.create({name: "Ademario Vitor", age: 23 })
+  console.log(user)
+}
 
-// run()
+run()
 
 app.use(cors({
   origin: '*'
 }));
 
-const port = process.env.PORT ||  4000
+const port = process.env.PORT ||  80
 
 app.use(router)
 
