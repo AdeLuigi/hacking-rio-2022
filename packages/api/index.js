fc3d4575f85f957fa4b952cb34006da11a88f1a2
Ademario Santana <ademario63@gmail.com>
2022-08-29 11:53:16 -0300

    fix: solve error in heroku

--- packages/api/index.js
+++ packages/api/index.js
@@ -2,23 +2,23 @@ const express = require('express')
 const router = require('./routes/routes')
 const cors = require('cors')
 const app = express()
-// const mongoose = require('mongoose')
-// const User = require('./schemas/User.schema')
+const mongoose = require('mongoose')
+const User = require('./schemas/User.schema')
 
-// mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
-//   console.log("conectou no mongo")
-// }, e => console.log(e))
+mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
+  console.log("conectou no mongo")
+}, e => console.log(e))
 
-// async function run() {
-//   const user = await User.create({name: "Ademario Vitor", age: 23 })
-//   console.log(user)
-// }
+async function run() {
+  const user = await User.create({name: "Ademario Vitor", age: 23 })
+  console.log(user)
+}
 
-// run()
+run()
 
-// app.use(cors({
-//   origin: '*'
-// }));
+app.use(cors({
+  origin: '*'
+}));
 
 const port = process.env.PORT ||  80
 
