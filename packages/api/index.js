8491e7939e531559b06a2d330d44f1dce1f7694e
Ademario Santana <ademario63@gmail.com>
2022-08-29 11:41:14 -0300

    test

--- packages/api/index.js
+++ packages/api/index.js
@@ -5,9 +5,9 @@ const app = express()
 const mongoose = require('mongoose')
 const User = require('./schemas/User.schema')
 
-mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
-  console.log("conectou no mongo")
-}, e => console.log(e))
+// mongoose.connect("mongodb+srv://admin:"+process.env.MONGO_PASSWORD+"@cluster0.ws0tq.mongodb.net/test", () => {
+//   console.log("conectou no mongo")
+// }, e => console.log(e))
 
 async function run() {
   const user = await User.create({name: "Ademario Vitor", age: 23 })
