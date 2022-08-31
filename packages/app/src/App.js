aafcb464cc15c2b122a11cb149cd90d830fbdb4a
Ademario Santana <ademario63@gmail.com>
2022-08-31 09:20:36 -0300

    feat: connect client with api and list users

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import api from './services/api'
 
 function App() {
+  const [users, setUsers] = useState([])
+  
+  useEffect(() => {
+      async function getUsers(params) {
+        const users = await api.get('/user')
+        setUsers(users.data)
+        console.log(users.data)
+      }
+      getUsers();
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {
+            users.map((user) => <p>{user.name}</p>)
+          }
       </header>
     </div>
   );
